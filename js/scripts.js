399b521de1417c1ae94d53e23ef129a523b1b10c
Mahmoud Elganzory <mbuyer7@gmail.com>
2024-06-06 00:39:40 +0800

    added the script for emailing and stuff

--- js/scripts.js
+++ js/scripts.js
@@ -36,4 +36,23 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
+const btn = document.getElementById('button');
 
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'service_rinx9d4';
+   const templateID = 'template_keurcys';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
