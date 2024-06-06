63075537b90993752f7e9185e398fc185e8cb900
Mahmoud Elganzory <mbuyer7@gmail.com>
2024-06-06 09:18:41 +0800

    Finalising The Project

--- js/scripts.js
+++ js/scripts.js
@@ -1,58 +1,21 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const expandButtons = document.querySelectorAll('.expand-btn');
 
-    expandButtons.forEach(button => {
-        button.addEventListener('click', (event) => {
-            const project = event.target.closest('.project');
-            const projectDetails = project.querySelector('.project-details');
 
-            if (project.classList.contains('expanded')) {
-                project.classList.remove('expanded');
-                projectDetails.style.display = 'none';
-            } else {
-                project.classList.add('expanded');
-                projectDetails.style.display = 'block';
-            }
+    const btn = document.getElementById('button');
 
-            
-        });
+    document.getElementById('form').addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        btn.value = 'Sending...';
+
+        const serviceID = 'service_rinx9d4';
+        const templateID = 'template_keurcys';
+
+        emailjs.sendForm(serviceID, templateID, this)
+            .then(() => {
+                btn.value = 'Send Email';
+                alert('Sent!');
+            }, (err) => {
+                btn.value = 'Send Email';
+                alert(JSON.stringify(err));
+            });
     });
-
-    expandButtons.forEach(button => {
-        button.addEventListener('click', (event) => {
-            const project2 = event.target.closest('.project2');
-            const projectDetails2 = project2.querySelector('.project-details2');
-
-            if (project2.classList.contains('expanded')) {
-                project2.classList.remove('expanded');
-                projectDetails2.style.display = 'none';
-            } else {
-                project2.classList.add('expanded');
-                projectDetails2.style.display = 'block';
-            }
-
-            
-        });
-    });
-});
-
-const btn = document.getElementById('button');
-
-document.getElementById('form')
- .addEventListener('submit', function(event) {
-   event.preventDefault();
-
-   btn.value = 'Sending...';
-
-   const serviceID = 'service_rinx9d4';
-   const templateID = 'template_keurcys';
-
-   emailjs.sendForm(serviceID, templateID, this)
-    .then(() => {
-      btn.value = 'Send Email';
-      alert('Sent!');
-    }, (err) => {
-      btn.value = 'Send Email';
-      alert(JSON.stringify(err));
-    });
-});
