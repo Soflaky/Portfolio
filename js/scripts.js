daf4f1181bbe9c2f1f364674391236d7744cadc2
Mahmoud Elganzory <mbuyer7@gmail.com>
2024-08-09 15:25:02 +0300

    pulling

--- js/scripts.js
+++ js/scripts.js
@@ -19,4 +19,10 @@
                 alert(JSON.stringify(err));
             });
     });
-
+  /*  document.addEventListener('DOMContentLoaded', function () {
+        const themeToggleBtn = document.getElementById('theme-toggle');
+    
+        themeToggleBtn.addEventListener('click', function () {
+            document.body.classList.toggle('light-mode');
+        });
+    }); */  // theme related script
